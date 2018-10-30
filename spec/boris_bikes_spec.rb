ad91057bad242abbb9e5bd5c3fc561ce245c01fc
leoncross <leon@lscross.co.uk>
2018-10-30 16:18:09 +0000

    added two tests, inc. one liner

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,9 +1,11 @@
 require "docking_station"
 
 describe DockingStation do
-
   it "releases bike" do
-    expect(DockingStation.new).to respond_to(:release_bike)
+    expect(subject).to respond_to(:release_bike)
   end
+end
 
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike}
 end
